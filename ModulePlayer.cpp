7097837872033ec76566537bee49e24dbafdeaab
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2023-11-11 19:07:04 +0100

    movimiento pasado

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModulePlayer.h"
 
+#define FRICTION 0.1f
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
@@ -60,7 +61,226 @@ update_status ModulePlayer::Update()
 		}
 		
 	}
+	if (App->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) {
+		movOption++;
+		if (movOption > 6) {
+			movOption = 0;
+		}
+	}
 
+	switch (movOption) {
+	case 0:
+		test = movementOptions::MOVX;
+		LOG("--------------------USING MOVX");
+		break;
+	case 1:
+		test = movementOptions::FIXVEL;
+		LOG("--------------------USING FIXVEL");
+		break;
+	case 2:
+		test = movementOptions::FIXACC;
+		LOG("--------------------USING FIXACC");
+		break;
+	case 3:
+		test = movementOptions::MOMENTUM;
+		LOG("--------------------USING MOMENTUM");
+		break;
+	case 4:
+		test = movementOptions::IMPULSE;
+		LOG("--------------------USING IMPULSE");
+		break;
+	case 5:
+		test = movementOptions::ACCELERATION;
+		LOG("--------------------USING ACCELERATION");
+		break;
+	case 6:
+		test = movementOptions::FORCE;
+		LOG("--------------------USING FORCE");
+		break;
+
+	}
+
+	switch (test)
+	{
+	case ModulePhysics::MOVX:
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x += 10;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (!front)
+			{
+				front = true;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x < SCREEN_WIDTH - 101)
+			{
+				cannonpos++;
+			}
+		}
+
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x -= 10;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (front)
+			{
+				front = false;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x > 0)
+			{
+				testCannon.x--;
+			}
+		}
+		break;
+	case ModulePhysics::FIXVEL:
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x += testCannon.vx;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (!front)
+			{
+				front = true;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x < SCREEN_WIDTH - 101)
+			{
+				testCannon.x++;
+			}
+		
+
+		}
+
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x -= testCannon.vx;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (front)
+			{
+				front = false;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x > 0)
+			{
+				testCannon.x--;
+			}
+		}
+		break;
+	case ModulePhysics::FIXACC:
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x += testCannon.vx;
+			testCannon.vx += testCannon.ax;
+
+			bullet.x = testCannon.x + testCannon.w;
+			if (!front)
+			{
+				front = true;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x < SCREEN_WIDTH - 101)
+			{
+				testCannon.x++;
+			}
+		}
+
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && App->physics->angle <= 0) {
+			if (testCannon.vx > 0) {
+				testCannon.x -= testCannon.vx;
+			}
+			else {
+				testCannon.x += testCannon.vx;
+			}
+
+			testCannon.vx -= testCannon.ax;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (front)
+			{
+				front = false;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x > 0)
+			{
+				testCannon.x--;
+			}
+		}
+		break;
+	case ModulePhysics::MOMENTUM:
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.x += testCannon.vx;
+			testCannon.vx += testCannon.ax;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (!front)
+			{
+				front = true;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x < SCREEN_WIDTH - 101)
+			{
+				testCannon.x++;
+			}
+		}
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && App->physics->angle <= 0) {
+			if (testCannon.vx > 0) {
+				testCannon.x -= testCannon.vx;
+			}
+			else {
+				testCannon.x += testCannon.vx;
+			}
+			testCannon.vx -= testCannon.ax;
+			bullet.x = testCannon.x + testCannon.w;
+
+			if (front)
+			{
+				front = false;
+				App->physics->angle = -180 - App->physics->angle;
+			}
+			if (testCannon.x > 0)
+			{
+				testCannon.x--;
+			}
+		}
+		// Apply friction
+		testCannon.vx -= (testCannon.vx > 0) ? FRICTION : (testCannon.vx < 0) ? -FRICTION : 0.0f;
+		break;
+	case ModulePhysics::IMPULSE:
+
+		break;
+	case ModulePhysics::ACCELERATION:
+		break;
+	case ModulePhysics::FORCE:
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_UP || App->input->GetKey(SDL_SCANCODE_A) == KEY_UP) {
+			testCannon.ax = 0;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.ax = 1;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && App->physics->angle <= 0) {
+			testCannon.ax = -1;
+
+		}
+
+		testCannon.ax = testCannon.ax / testCannon.m;
+		testCannon.vx += testCannon.ax;
+		// Apply friction
+		if (testCannon.vx > 0)
+		{
+			testCannon.vx -= FRICTION;
+			if (testCannon.vx < 0)
+				testCannon.vx = 0;
+		}
+		else if (testCannon.vx < 0)
+		{
+			testCannon.vx += FRICTION;
+			if (testCannon.vx > 0)
+				testCannon.vx = 0;
+		}
+		testCannon.x += testCannon.vx;
+		bullet.x = testCannon.x + testCannon.w;
+		break;
+
+	}
 	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && App->physics->angle <= 0)
 	{
 
--- ModulePlayer.h
+++ ModulePlayer.h
@@ -18,7 +18,9 @@ class ModulePlayer : public Module
 public:
 	ModulePlayer(Application* app, bool start_enabled = true);
 	virtual ~ModulePlayer();
-
+	bool Start();
+	update_status Update();
+	bool CleanUp();
 	SDL_Texture* cannon = nullptr;
 
 	SDL_Texture* ball = nullptr;
@@ -26,10 +28,117 @@ public:
 	int cannonpos = 0;
 	bool front = true;
 
-	bool Start();
-	update_status Update();
-	bool CleanUp();
+	enum movementOptions
+	{
+		MOVX,
+		FIXVEL,
+		FIXACC,
+		MOMENTUM,
+		IMPULSE,
+		ACCELERATION,
+		FORCE
+	};
+	movementOptions test = movementOptions::MOVX;
+
+	class player
+	{
+	public:
+		int x;
+		int y;
+		int w;
+		int h;
+
+		double v;
+		double vx;
+		double vy;
+
+		float ax;
+		int ay;
+
+		int m;
+
+		player() {
+			x = 80;
+			y = 560;
+			w = 50;
+			h = 80;
+			v = 30;
+			vx = 1;
+			vy = 20;
+			ax = 2;
+			ay = 1;
+			m = 5;
+
+		}
+
+		player(int posX, int posY, int width, int height, double vel, double velX, double velY, float accX, int accY, int mass) {
+			x = posX;
+			y = posY;
+			w = width;
+			h = height;
+			v = vel;
+			vx = velX;
+			vy = velY;
+			ax = accX;
+			ay = accY;
+			m = mass;
+		}
+	};
+
+	class bullet
+	{
+	public:
+		int x;
+		int y;
+
+		int r;
+
+		double angle;
+
+		double v;
+		double vx;
+		double vy;
+
+		int ax;
+		int ay;
+
+		int m;
+
+
+
+		bullet() {
+			x = 80;
+			y = 550;
+			r = 20;
+			angle = 30;
+			v = 25;
+			vx = v * cos(angle * DEGTORAD);
+			vy = -v * sin(angle * DEGTORAD);
+			ax = 0;
+			ay = 1;
+			m = 5;
+		}
+
+		bullet(int posX, int posY, int radius, int a, double vel, double velX, double velY, int accX, int accY, int mass) {
+			x = posX;
+			y = posY;
+			r = radius;
+			angle = a;
+			v = vel;
+			vx = velX;
+			vy = velY;
+			ax = accX;
+			ay = accY;
+			m = mass;
+		}
+	};
+	
 
 public:
-
+	
+private:
+	int movOption = 0;
+public:
+	bullet bullet;
+	player testCannon;
 };
